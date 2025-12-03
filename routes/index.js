9c08dabf373df6fb0ee090fd20d400327fd457fc
Fernando-Falla <livesurfer01@gmail.com>
2025-12-03 17:31:26 -0500

    Delete routes directory

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-// Home
-router.get('/', (req, res) => {
-  res.render('index', { title: 'Doguito API' });
-});
-
-module.exports = router;
